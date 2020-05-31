66e33f4452189e2c03e25c56b05aefe2d32b4a56
alexis <alexis@hackages.io>
2020-05-31 18:06:54 +0200

    Add new theme

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Cityton',
-      theme: appTheme(),
+      theme: appTheme,
       initialRoute: '/',
       routes: routes,
     );
