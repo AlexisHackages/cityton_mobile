baa9a017da6bfbe4fbe8b9c74b694e9e7bbee816
alexis <alexis@hackages.io>
2020-06-22 17:10:15 +0200

    change snackbar position

--- lib/components/DisplaySnackbar.dart
+++ lib/components/DisplaySnackbar.dart
@@ -2,37 +2,33 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
 class DisplaySnackbar {
-
   static void createError(
       {@required String message,
       String title,
       Duration duration = const Duration(seconds: 10)}) {
-    return Get.snackbar(
-      title,
-      message,
-      icon: Icon(
-        Icons.warning,
-        size: 28.0,
-        color: Colors.red[300],
-      ),
-      leftBarIndicatorColor: Colors.red[300],
-      duration: duration);
+    return Get.snackbar(title, message,
+        icon: Icon(
+          Icons.warning,
+          size: 28.0,
+          color: Colors.red[300],
+        ),
+        leftBarIndicatorColor: Colors.red[300],
+        duration: duration,
+        snackPosition: SnackPosition.BOTTOM);
   }
 
   static void createConfirmation(
       {@required String message,
       String title,
       Duration duration = const Duration(seconds: 10)}) {
-    return Get.snackbar(
-      title,
-      message,
-      icon: Icon(
-        Icons.warning,
-        size: 28.0,
-        color: Colors.green[300],
-      ),
-      leftBarIndicatorColor: Colors.green[300],
-      duration: duration,
-    );
+    return Get.snackbar(title, message,
+        icon: Icon(
+          Icons.warning,
+          size: 28.0,
+          color: Colors.green[300],
+        ),
+        leftBarIndicatorColor: Colors.green[300],
+        duration: duration,
+        snackPosition: SnackPosition.BOTTOM);
   }
 }
